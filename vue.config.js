ed6c089efe6c113043aad7b556989edd057fdd70
Manuel Meister <news.manuelsworld@gmail.com>
2020-04-29 11:42:27 +0200

    Improved loading

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
+
 module.exports = {
     chainWebpack: config => {
         config.module
@@ -5,6 +7,12 @@ module.exports = {
             .use('url-loader')
             .loader('url-loader')
             .tap(options => Object.assign(options, {limit: 10240}))
+
+        config.plugin('PreloadWebpackPlugin').use(PreloadWebpackPlugin,[{
+            rel: 'preload',
+            include: 'allAssets',
+            fileWhitelist:[/\.woff2/],
+        }])
     },
     publicPath: '/',
     transpileDependencies: [
